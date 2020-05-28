df19b6aa6b1927d6522ab9f33439d4f3b29a126a
Opryatnov Dmitriy <apratnau_ds@st.by>
2020-05-28 11:20:46 +0300

    Create project

---
+++ Currency Converter/Currency Converter/SceneDelegate.swift
@@ -0,0 +1,15 @@
+//
+//  SceneDelegate.swift
+//  Currency Converter
+//
+//  Created by Opryatnov Dmitriy on 5/28/20.
+//  Copyright © 2020 Opryatnov Dmitriy. All rights reserved.
+//
+
+import UIKit
+
+class SceneDelegate: UIResponder, UIWindowSceneDelegate {
+
+    var window: UIWindow?
+}
+
