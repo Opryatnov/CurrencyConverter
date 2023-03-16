2a371431782fac83d399e2b6cc01626be3ed432d
Dmitriy Opryatnov <opryatnov.dmitriy@gmail.com>
2023-03-16 15:11:33 +0300

    [Add]: 1. Tab bar 2. Customize tab bar 3. Controllers 4. Tab bar icons (selected/unselected)

---
+++ CurrencyConverter/CurrencyConverter/Extensions/UIColor+Extension.swift
@@ -0,0 +1,22 @@
+//
+//  UIColor+Extension.swift
+//  CurrencyConverter
+//
+//  Created by Dmitriy Opryatnov on 16.03.23.
+//
+
+import UIKit
+
+extension UIColor {
+    static var tabBarItemAccent: UIColor {
+        UIColor.systemCyan
+    }
+    
+    static var mainWhite: UIColor {
+        UIColor.white
+    }
+    
+    static var tabBarItemLight: UIColor {
+        UIColor.gray
+    }
+}
