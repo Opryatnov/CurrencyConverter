4c39a023f9acb7026ab530cd9c8fec4191eb9c4c
Dmitriy Opryatnov <dmitry.opriatnov@fmobile.by>
2024-05-10 16:52:08 +0300

    added fuel cell added new colours added new background

---
+++ CurrencyConverter/CurrencyConverter/Controllers/FelCalculatorViewController/FuelCalculatorViewController.swift
@@ -0,0 +1,84 @@
+import UIKit
+
+final class FuelCalculatorViewController: UIViewController {
+    
+    // MARK: Constants
+    
+    private enum Constants {
+        static let tableViewBottomInset: CGFloat = 20
+        static let tableViewAdditionalInset: CGFloat = 15
+        static let tableViewContentInset: CGFloat = 20
+    }
+    
+    // MARK: UI
+    
+    private let tableView: UITableView = {
+        let tableView = UITableView(frame: .zero, style: .grouped)
+        tableView.separatorStyle = .none
+//        tableView.backgroundColor = .clear
+        tableView.showsVerticalScrollIndicator = false
+        tableView.showsHorizontalScrollIndicator = false
+        tableView.backgroundColor = UIColor(resource: .darkGray6)
+        
+        return tableView
+    }()
+    
+    // MARK: Private properties
+    
+    private var fuelList: [Fuel]? = []
+    
+    // MARK: Lifecycle
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+//        view.addGradient([UIColor.green, UIColor.yellow, UIColor.red], locations: [0.0, 0.5, 1.0],frame: view.frame)
+        view.backgroundColor = UIColor(resource: .darkGray6)
+        view.addSubview(tableView)
+        tableView.snp.makeConstraints {
+            $0.leading.trailing.equalToSuperview()
+            $0.top.equalToSuperview().inset(Constants.tableViewBottomInset)
+            $0.bottom.equalToSuperview().inset((self.tabBarController?.tabBar.frame.height ?? .zero) + Constants.tableViewAdditionalInset)
+        }
+        tableView.contentInset.bottom = Constants.tableViewContentInset
+        tableView.delegate = self
+        tableView.dataSource = self
+        tableView.register(FuelCalculatorCell.self, forCellReuseIdentifier: FuelCalculatorCell.identifier)
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        NetworkService.shared.fetchFuel { result in
+            switch result {
+            case .success(let fuelList):
+                self.fuelList = fuelList
+                self.tableView.reloadData()
+            case .failure(let error):
+                self.showAlert(message: error.localizedDescription, buttons: [], viewController: self)
+            }
+        }
+    }
+}
+
+// MARK: - UITableViewDelegate, - UITableViewDataSource
+
+extension FuelCalculatorViewController: UITableViewDataSource, UITableViewDelegate {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        fuelList?.count ?? .zero
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: FuelCalculatorCell.identifier, for: indexPath) as? FuelCalculatorCell
+        cell?.selectionStyle = .none
+        if let fuel = fuelList?[indexPath.row] {
+            cell?.fill(fuel: fuel)
+        }
+        return cell ?? UITableViewCell()
+    }
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+//        if let currency = currencies?[indexPath.row] {
+//            currency.isSelected.toggle()
+//            tableView.reloadData()
+//        }
+    }
+}
--- CurrencyConverter/CurrencyConverter/Network/Models/Fuel.swift
+++ CurrencyConverter/CurrencyConverter/Network/Models/Fuel.swift
@@ -13,4 +13,27 @@ struct Fuel: Decodable {
     let fuelName_ENG: String?
     let fuelName_BEL: String?
     let cost: Double?
+    
+    var localisedName: String {
+        if #available(iOS 16, *) {
+            return setFuelName(language: Locale.current.language.languageCode?.identifier ?? "")
+        } else {
+            return setFuelName(language: Locale.current.identifier)
+        }
+    }
+    
+    func setFuelName(language: String) -> String {
+        var tempFuelName: String?
+        switch language {
+        case "be":
+            tempFuelName = fuelName_BEL
+        case "en":
+            tempFuelName = fuelName_ENG
+        case "ru":
+            tempFuelName = fuelName_RU
+        default:
+            tempFuelName = fuelName_ENG
+        }
+        return tempFuelName ?? ""
+    }
 }
